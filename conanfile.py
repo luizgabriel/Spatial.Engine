ebfa0945659447de4af737f732315b45da63cbba
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-06-07 14:04:17 -0300

    Updated conanfile.txt to conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,48 @@
+from conans import ConanFile, CMake
+
+
+class SpatialEngineConan(ConanFile):
+    name = "spatial-engine"
+    version = "1.0.0"
+    license = "Apache 2.0"
+    url = "https://github.com/luizgabriel/Spatial.Engine"
+    description = "Spatial is a cross-platform c++ game engine created on top off google's [filament](" \
+                  "https://github.com/google/filament) rendering engine. This projects uses C++17 and modern cmake " \
+                  "features. "
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"sandbox": [True, False]}
+    default_options = {"sandbox": False}
+    generators = "cmake"
+    build_requires = "cmake_installer/3.16.3@conan/stable"
+    requires = [
+        "entt/3.4.0@skypjack/stable",
+        "fmt/6.0.0@bincrafters/stable",
+        "spdlog/1.4.2@bincrafters/stable",
+        "stb/20190512@conan/stable",
+        "boost_tti/1.67.0@bincrafters/stable",
+        "sdl2/2.0.12@bincrafters/stable",
+        "filament/1.7.0@google/stable",
+        "imgui/docking@ocornut/testing",
+        "toml++/1.2.0@marzer/stable",
+    ]
+    imports = [
+        "filament:bin, * -> ./bin"
+    ]
+
+    def _cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["SPATIAL_SANDBOX"] = self.options["sandbox"]
+
+        return cmake
+
+    def build(self):
+        cmake = self._cmake()
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = self._cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = self.collect_libs()
--- scripts/setup_dependencies.py
+++ scripts/setup_dependencies.py
@@ -31,18 +31,22 @@ def conan_install():
     return run("cd %s && conan install .. -pr %s -b missing" % (CONAN_DIR, DEFAULT_PROFILE))
 
 
-if __name__ == "__main__":
-    try:
-        os.makedirs(CONAN_DIR)
-    except:
-        pass
-
+def export_dependencies():
     conan_repo("bincrafters", "https://api.bintray.com/conan/bincrafters/public-conan")
 
     conan_export("luizgabriel", "conan-filament", "filament/1.7.0@google/stable", "v1.7.0")
     conan_export("luizgabriel", "conan-imgui", "imgui/docking@ocornut/testing", "docking")
     conan_export("luizgabriel", "conan-tomlplusplus", "toml++/1.2.0@marzer/stable")
     conan_export("skypjack", "entt", "entt/3.4.0@skypjack/stable", "v3.4.0")
+
+
+if __name__ == "__main__":
+    try:
+        os.makedirs(CONAN_DIR)
+    except:
+        pass
+
+    export_dependencies()
 
     if conan_install() == 0:
         print("Done! Spatial Engine is now ready for compilation")
