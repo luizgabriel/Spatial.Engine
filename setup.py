32e6c2ebcc7357a1e71a1628a4dd6b8c277013b5
Luiz Gabriel <luizgabriel.info@gmail.com>
2021-08-30 12:40:42 -0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,9 @@ def conan_export(package_dir, export_name):
     return run("conan export %s %s" % (package_dir, export_name))
 
 
-def conan_add_remote(remote_name, remote_url):
-    return run("conan remote add %s %s" % (remote_name, remote_url))
-
-
 def install(folder):
     install_folder = os.path.abspath(folder)
     vendor_path = os.path.join(install_folder, "vendor")
-
-    conan_add_remote("bincrafters", "https://api.bintray.com/conan/bincrafters/public-conan")
 
     conan_export(os.path.join(vendor_path, "filament.py"), "filament/1.9.21@vendor/stable")
     conan_export(os.path.join(vendor_path, "imgui.py"), "imgui/docking@vendor/stable")
