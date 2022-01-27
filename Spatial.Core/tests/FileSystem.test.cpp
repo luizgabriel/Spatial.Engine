63cbd678aa747996e5ef71642f83bb75e11dc6aa
Luiz Gabriel <luizgabriel.info@gmail.com>
2022-01-27 17:24:24 -0300

    Fix filesystem tests

--- Spatial.Core/tests/FileSystem.test.cpp
+++ Spatial.Core/tests/FileSystem.test.cpp
@@ -10,15 +10,15 @@ TEST(FileSystem, Resolve)
 	auto mem = root.mount<MemoryFileSystem>("mem");
 
 	{
-		auto result = root->resolve("");
+		auto result = root.resolve("");
 		EXPECT_EQ(result.path, "");
-		EXPECT_EQ(result.fileSystem, root);
+		EXPECT_EQ(&result.fileSystem, &root);
 	}
 
 	{
-		auto result = root->resolve("mem");
+		auto result = root.resolve("mem");
 		EXPECT_EQ(result.path, "");
-		EXPECT_EQ(result.fileSystem, *mem);
+		EXPECT_EQ(&result.fileSystem, &*mem);
 	}
 }
 
@@ -30,19 +30,15 @@ TEST(FileSystem, OpenFileForRead)
 	auto mem = root.mount<MemoryFileSystem>("m1");
 	mem->define("memory_test.txt", "Hello, World! From Memory");
 
-	EXPECT_THROW(root.readString("v1/unknown.txt"), std::invalid_argument);
+	EXPECT_EQ(root.readString("v1/unknown.txt"), "");
 
-	auto value1 = root.readString("v1/test.txt");
-	EXPECT_EQ(value1, "Hello, World! Inside A2");
+	EXPECT_EQ(root.readString("v1/test.txt"), "Hello, World! Inside A2");
 
-	auto value2 = root.readString("v2/a1/b1/test.txt");
-	EXPECT_EQ(value2, "Hello, World! Inside A1/B1");
+	EXPECT_EQ(root.readString("v2/a1/b1/test.txt"), "Hello, World! Inside A1/B1");
 
-	auto value3 = root.readString("b1/test.txt");
-	EXPECT_EQ(value3, "Hello, World! Inside A1/B1");
+	EXPECT_EQ(root.readString("b1/test.txt"), "Hello, World! Inside A1/B1");
 
-	auto value4 = root.readString("m1/memory_test.txt");
-	EXPECT_EQ(value4, "Hello, World! From Memory");
+	EXPECT_EQ(root.readString("m1/memory_test.txt"), "Hello, World! From Memory");
 }
 
 TEST(FileSystem, OpenFileForWrite)
@@ -52,8 +48,7 @@ TEST(FileSystem, OpenFileForWrite)
 
 	root.write("v1/test_write.txt", "Test Write");
 
-	auto value = root.readString("v1/test_write.txt");
-	EXPECT_EQ(value, "Test Write");
+	EXPECT_EQ(root.readString("v1/test_write.txt"), "Test Write");
 }
 
 TEST(FileSystem, ListPaths)
@@ -65,32 +60,32 @@ TEST(FileSystem, ListPaths)
 	mem->define("folder/memory2.txt", "1245");
 
 	{
-		auto result = root.listImpl("v1/");
+		auto result = root.list("v1/");
 		EXPECT_EQ(result.size(), 1);
 		EXPECT_TRUE(result.find({"test.txt", FileSystem::FileType::File}) != result.end());
 	}
 
 	{
-		auto result = root.listImpl("v1");
+		auto result = root.list("v1");
 		EXPECT_EQ(result.size(), 1);
 		EXPECT_TRUE(result.find({"test.txt", FileSystem::FileType::File}) != result.end());
 	}
 
 	{
-		auto result = root.listImpl("m1");
+		auto result = root.list("m1");
 		EXPECT_EQ(result.size(), 2);
 		EXPECT_TRUE(result.find({"memory.txt", FileSystem::FileType::File}) != result.end());
 		EXPECT_TRUE(result.find({"folder", FileSystem::FileType::Directory}) != result.end());
 	}
 
 	{
-		auto result = root.listImpl("m1/folder");
+		auto result = root.list("m1/folder");
 		EXPECT_EQ(result.size(), 1);
 		EXPECT_TRUE(result.find({"memory2.txt", FileSystem::FileType::File}) != result.end());
 	}
 
 	{
-		auto result = root.listImpl("");
+		auto result = root.list("");
 		EXPECT_EQ(result.size(), 4);
 		EXPECT_TRUE(result.find({"a1", FileSystem::FileType::Directory}) != result.end());
 		EXPECT_TRUE(result.find({"a2", FileSystem::FileType::Directory}) != result.end());
