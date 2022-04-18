d935f3dbc188a8981a68db55e1bea4e50ed77d76
Luiz Gabriel <luizgabriel.info@gmail.com>
2022-04-18 11:05:43 -0300

    Fix build for windows

--- Spatial.Core/include/spatial/serialization/Name.h
+++ Spatial.Core/include/spatial/serialization/Name.h
@@ -12,19 +12,4 @@ void serialize(Archive& ar, spatial::ecs::Name& node)
 	ar(make_nvp("name", node.name));
 }
 
-template <typename Archive>
-void save(Archive& ar, const spatial::ecs::UUID& id)
-{
-	ar(id.toString());
-}
-
-template <typename Archive>
-void load(Archive& ar, spatial::ecs::UUID& id)
-{
-	std::string uuid;
-	ar(uuid);
-	if (uuid.size() > 0)
-		id = spatial::ecs::UUID::fromString(uuid);
-}
-
 } // namespace cereal
