570a940329c7f8c4f484d67183ce48f273f185ac
Luiz Gabriel <luizgabriel.info@gmail.com>
2022-05-10 01:04:53 -0300

    Update installation presets for windows. Update LICENSE

--- CMake/PackageSettings.cmake
+++ CMake/PackageSettings.cmake
@@ -1,3 +1,16 @@
+set(CPACK_PACKAGE_VENDOR "Luiz Gabriel (luizgabriel.info@gmail.com)")
+set(CPACK_PACKAGE_DESCRIPTION "Spatial is a cross-platform C++ game engine created on top off google's filament rendering engine. This projects uses C++17 and modern cmake features.")
+set(CMAKE_PROJECT_HOMEPAGE_URL "https://luizgabriel.github.io/Spatial.Engine/")
+set(CPACK_VERBATIM_VARIABLES TRUE)
+set(CPACK_CREATE_DESKTOP_LINKS TRUE)
+set(CPACK_DMG_BACKGROUND_IMAGE ${PROJECT_SOURCE_DIR}/Spatial.Editor/assets/BundleBackground.png)
+set(CPACK_PACKAGE_ICON ${PROJECT_SOURCE_DIR}/Spatial.Editor/assets/AppIcon.bmp)
+set(CPACK_WIX_PRODUCT_ICON ${PROJECT_SOURCE_DIR}/Spatial.Editor/assets/AppIcon.ico)
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+set(CPACK_RESOURCE_FILE_LICENSE ${PROJECT_SOURCE_DIR}/LICENSE.txt)
+
 include(CPack)
 
 cpack_add_component(binaries
@@ -12,15 +25,3 @@ cpack_add_component(headers
         DISPLAY_NAME "C++ Headers"
         DESCRIPTION "C/C++ header files for using the Spatial Core"
         GROUP Development)
-
-set(CPACK_PACKAGE_VENDOR "Luiz Gabriel (luizgabriel.info@gmail.com)")
-set(CPACK_PACKAGE_DESCRIPTION "Spatial is a cross-platform C++ game engine created on top off google's filament rendering engine. This projects uses C++17 and modern cmake features.")
-set(CMAKE_PROJECT_HOMEPAGE_URL "https://luizgabriel.github.io/Spatial.Engine/")
-set(CPACK_VERBATIM_VARIABLES TRUE)
-set(CPACK_CREATE_DESKTOP_LINKS TRUE)
-set(CPACK_DMG_BACKGROUND_IMAGE ${CMAKE_CURRENT_SOURCE_DIR}/Spatial.Editor/assets/BundleBackground.png)
-set(CPACK_PACKAGE_ICON ${CMAKE_CURRENT_SOURCE_DIR}/Spatial.Editor/assets/AppIcon.bmp)
-set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
-set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
-set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
-set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
