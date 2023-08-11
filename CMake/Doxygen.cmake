f4a156b7c0572403665ad49680a14d467fe71d7d
Luiz Gabriel <luizgabriel.info@gmail.com>
2023-08-11 11:22:34 -0300

    Update Doxygen.cmake

--- CMake/Doxygen.cmake
+++ CMake/Doxygen.cmake
@@ -1,19 +1,23 @@
-set(DOXYGEN_CALLER_GRAPH YES)
-set(DOXYGEN_CALL_GRAPH YES)
-set(DOXYGEN_COLLABORATION_GRAPH YES)
-set(DOXYGEN_EXTRACT_ALL YES)
-set(DOXYGEN_CLASS_DIAGRAMS YES)
-set(DOXYGEN_HIDE_UNDOC_RELATIONS NO)
-set(DOXYGEN_HAVE_DOT YES)
-set(DOXYGEN_CLASS_GRAPH YES)
-set(DOXYGEN_USE_MDFILE_AS_MAINPAGE ${PROJECT_SOURCE_DIR}/README.md)
+option(SPATIAL_ENABLE_DOXYGEN "Enable Doxygen documentation builds of source." OFF)
 
-find_package(Doxygen REQUIRED dot)
-doxygen_add_docs(Spatial.Docs
-        ${PROJECT_SOURCE_DIR}/README.md
-        ${PROJECT_SOURCE_DIR}/Spatial.Core
-        ${PROJECT_SOURCE_DIR}/Spatial.Graphics
-        ${PROJECT_SOURCE_DIR}/Spatial.Res
-        ${PROJECT_SOURCE_DIR}/Spatial.UI
-        ${PROJECT_SOURCE_DIR}/Spatial.Editor
-        COMMENT "Generating doxygen for Spatial.Engine")
+if (SPATIAL_ENABLE_DOXYGEN)
+    set(DOXYGEN_CALLER_GRAPH YES)
+    set(DOXYGEN_CALL_GRAPH YES)
+    set(DOXYGEN_COLLABORATION_GRAPH YES)
+    set(DOXYGEN_EXTRACT_ALL YES)
+    set(DOXYGEN_CLASS_DIAGRAMS YES)
+    set(DOXYGEN_HIDE_UNDOC_RELATIONS NO)
+    set(DOXYGEN_HAVE_DOT YES)
+    set(DOXYGEN_CLASS_GRAPH YES)
+    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE ${PROJECT_SOURCE_DIR}/README.md)
+
+    find_package(Doxygen REQUIRED dot)
+    doxygen_add_docs(Spatial.Docs
+            ${PROJECT_SOURCE_DIR}/README.md
+            ${PROJECT_SOURCE_DIR}/Spatial.Core
+            ${PROJECT_SOURCE_DIR}/Spatial.Graphics
+            ${PROJECT_SOURCE_DIR}/Spatial.Res
+            ${PROJECT_SOURCE_DIR}/Spatial.UI
+            ${PROJECT_SOURCE_DIR}/Spatial.Editor
+            COMMENT "Generating doxygen for Spatial.Engine")
+endif ()
