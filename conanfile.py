ffcde61ed70bb0a935a50d66f2760b0c6a26dfdd
Luiz Gabriel <luizgabriel.info@gmail.com>
2023-08-14 14:29:59 -0300

    Upgrade version 1.3.15

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.build import check_min_cppstd
 
 class SpatialRecipe(ConanFile):
     name = "spatial"
-    version = "1.3.14"
+    version = "1.3.15"
     package_type = "application"
 
     # Optional metadata
