e784ed5f2eb90d002249e638dccdebf10d1cdeed
Luiz Gabriel <luizgabriel.info@gmail.com>
2023-10-06 18:16:34 -0300

    Bump version 1.3.19

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.build import check_min_cppstd
 
 class SpatialRecipe(ConanFile):
     name = "spatial"
-    version = "1.3.18"
+    version = "1.3.19"
     package_type = "application"
 
     # Optional metadata
