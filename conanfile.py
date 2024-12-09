7fc603b5a88727c44c2bc113dbad9ea54c2299f5
Luiz Gabriel <luizgabriel.info@gmail.com>
2024-12-09 11:23:55 -0300

    Upgrade boost version

--- conanfile.py
+++ conanfile.py
@@ -49,7 +49,7 @@ class SpatialRecipe(ConanFile):
                 self.options.backend = "opengl"
 
     def requirements(self):
-        self.requires("boost/1.81.0")
+        self.requires("boost/1.86.0")
         self.requires("argh/1.3.1")
         self.requires("glfw/3.3.8")
         self.requires("gtest/1.11.0")
